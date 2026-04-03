d894bbe563cfe5e2a7f6be767f1ddff615f5e606
gunalukesh <gunalukesh@yahoo.in>
2026-04-04 02:12:10 +0530

    Add app/pwa-register.js

---
+++ app/pwa-register.js
@@ -0,0 +1,19 @@
+'use client'
+import { useEffect } from 'react'
+
+export function PWARegister() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((reg) => {
+          console.log('Appdemy SW registered:', reg.scope)
+        })
+        .catch((err) => {
+          console.log('SW registration failed:', err)
+        })
+    }
+  }, [])
+
+  return null
+}
